d048a4d1cd6b56e6207994ad00cd86c872de5164
leikeming <leikeming@pinduoduo.com>
2024-10-23 00:38:01 +0800

    重命名

--- src/test/java/chaogu/Last30DayInfoUtils.java
+++ src/test/java/chaogu/Last30DayInfoUtils.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class GetXiangDuiBiLi {
+public class Last30DayInfoUtils {
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
--- src/test/java/chaogu/Main.java
+++ src/test/java/chaogu/Main.java
@@ -15,19 +15,24 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
+    enum RunMode {
+        YuCe, JiaoYan;
+    }
+
+    RunMode runMode = RunMode.YuCe;
+
     static String lastDate = "2024-10-21";
+    static String todayDate = "2024-10-23";
     static double lastDapanStar2EndDiff = -0.25 / 100;
 
 //    原则：1 min 涨越多越好  2 有反弹更好 3 早上涨幅不能太高  4 非科技板块*2
 
     @Test
     public void main() throws IOException {
-        getBankuaiWithData(new BanKuai("沪深300", "90.BK0740"));
+//        getBankuaiWithData(new BanKuai("教育", "90.BK0740"));
         List<BanKuai> banKuaiList = parseAllBanKuai();
-//        System.out.println(JSON.toJSONString(banKuaiList));
         long starMs = System.currentTimeMillis();
         BankuaiWithData hushen300TodayWithData = getBankuaiWithData(new BanKuai("沪深300", "1.000300"));
         List<String> resultListt = banKuaiList.stream().parallel().map(e -> {
@@ -35,31 +40,10 @@ public class Main {
         }).sorted((a, b) -> {
             return (int) ((getSortValue(b) - getSortValue(a)) * 10000);
         }).map(e -> {
-            return String.format("板块：%-7s  " +
-                            //今日一分钟
-                            "\t 今日一分钟相对涨跌：%.3f%% " +
-                            "[即:%.3f%%]， \t  " +
-                            //今日开盘
-                            "今日开盘相对涨跌:%.3f%%" +
-                            " [即:%.3f%%] \t  " +
-                            //昨日
-                            " 上日相比大盘涨跌：%.2f%%" +
-                            " [即:%.2f%%]， " +
-                            //时间
-                            "\t  时间：%s",
-                    fillName(e.getBankuaiName()),
-                    //今日一分钟
-                    e.todayMinuteDataList.get(1).startEndDiff * 100 - hushen300TodayWithData.todayMinuteDataList.get(1).startEndDiff * 100,
-                    e.todayMinuteDataList.get(1).startEndDiff * 100,
-                    //今日开盘
-                    e.last2StartDiff * 100 - hushen300TodayWithData.last2StartDiff * 100,
-                    e.last2StartDiff * 100,
-                    //昨日
-                    (e.lastDayDetail.startEndDiff - lastDapanStar2EndDiff) * 100,
-                    e.lastDayDetail.startEndDiff * 100,
-                    //时间
-                    e.todayMinuteDataList.get(1).dateTime
-            );
+            String ret = todayOneMinutteDesc(hushen300TodayWithData, e);
+            e.last30DayInfo.detailList.subList(e.last30DayInfo.detailList.size() - 30, e.last30DayInfo.detailList.size());
+            //30 天比例，最大比例，最小比例，昨日比例
+            return ret;
         }).collect(Collectors.toList());
         long endMs = System.currentTimeMillis();
         System.out.printf("开始时间：%s, 花费时间：%.2f s  \n" +
@@ -72,16 +56,43 @@ public class Main {
         resultListt.forEach(System.out::println);
     }
 
+    private static String todayOneMinutteDesc(BankuaiWithData hushen300TodayWithData, BankuaiWithData e) {
+        return String.format("板块：%-7s  " +
+                        //今日一分钟
+                        "\t 今日一分钟相对涨跌：%.3f%% " +
+                        "[即:%.3f%%]， \t  " +
+                        //今日开盘
+                        "今日开盘相对涨跌:%.3f%%" +
+                        " [即:%.3f%%] \t  " +
+                        //昨日
+                        " 上日相比大盘涨跌：%.2f%%" +
+                        " [即:%.2f%%]， " +
+                        //时间
+                        "\t  时间：%s",
+                fillName(e.getBankuaiName()),
+                //今日一分钟
+                e.todayMinuteDataList.get(1).startEndDiff * 100 - hushen300TodayWithData.todayMinuteDataList.get(1).startEndDiff * 100,
+                e.todayMinuteDataList.get(1).startEndDiff * 100,
+                //今日开盘
+                e.last2StartDiff * 100 - hushen300TodayWithData.last2StartDiff * 100,
+                e.last2StartDiff * 100,
+                //昨日
+                (e.lastDayDetail.startEndDiff - lastDapanStar2EndDiff) * 100,
+                e.lastDayDetail.startEndDiff * 100,
+                //时间
+                e.todayMinuteDataList.get(1).dateTime
+        );
+    }
+
     static String fillName(String name) {
         if (name.length() == 2) {
             return name.charAt(0) + "   " + name.charAt(1);
-//            return name + "    ";
         }
         return name;
     }
 
     @NotNull
-    private static BankuaiWithData getBankuaiWithData(BanKuai e) {
+    public static BankuaiWithData getBankuaiWithData(BanKuai e) {
         BankuaiWithData bankuaiWithData = new BankuaiWithData();
         bankuaiWithData.setBankuaiName(e.getName());
         try {
@@ -122,9 +133,9 @@ public class Main {
     }
 
     //天
-    private static GetXiangDuiBiLi.Last30DayInfo getLastDayData(String bankuaiCode) throws IOException {
+    private static Last30DayInfoUtils.Last30DayInfo getLastDayData(String bankuaiCode) throws IOException {
         JSONArray jsonArray = getDayData(bankuaiCode);
-        return GetXiangDuiBiLi.getLast30DayInfo(jsonArray);
+        return Last30DayInfoUtils.getLast30DayInfo(jsonArray);
     }
 
     @Data
@@ -134,7 +145,7 @@ public class Main {
         String bankuaiName;
         List<OneData> todayMinuteDataList;
         double last2StartDiff;//今日开盘涨跌
-        GetXiangDuiBiLi.Last30DayInfo last30DayInfo;
+        Last30DayInfoUtils.Last30DayInfo last30DayInfo;
         上证.Main.OneDayDataDetail lastDayDetail;
     }
 
