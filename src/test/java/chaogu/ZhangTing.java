e9580379a776fdff624f6e798eb1f688eb28b671
leikeming <leikeming@pinduoduo.com>
2024-12-02 01:23:20 +0800

    房地产

--- src/test/java/chaogu/ZhangTing.java
+++ src/test/java/chaogu/ZhangTing.java
@@ -1,6 +1,7 @@
 package chaogu;
 
 import com.alibaba.fastjson.JSONArray;
+import lombok.AllArgsConstructor;
 import org.springframework.util.StringUtils;
 import shangZheng.Utils;
 
@@ -18,67 +19,80 @@ public class ZhangTing {
             if (StringUtils.isEmpty(line)) {
                 continue;
             }
-            String arr[] = line.split("	");
+            String arr[] = line.split("\\s+");
             code2Name.put(arr[0], arr[1]);
         }
         System.out.println(code2Name);
-        List<List<shangZheng.Main.OneDayDataDetail>> list2list = code2Name.keySet().parallelStream().map(code -> {
+        List<BankuaiData> list2list = code2Name.keySet().parallelStream().map(code -> {
             return getLast30DayData(successCodePrefixMap.get(code.substring(0, 3)) + "." + code);
         }).filter(Objects::nonNull).collect(Collectors.toList());
         System.out.println(list2list.size());
 
-        //1.512200 房地产 ， 1.561600 消费电子
-        List<shangZheng.Main.OneDayDataDetail> etfList = getLast30DayData("1.512200");
-        List<shangZheng.Main.OneDayDataDetail> etf300List = getLast30DayData("1.561600");
+        //1.512200 房地产 ， 1.561600 消费电子 ,  1.515790 光伏 , 1.516020 化工 ， 食品 1.515170 , 1.515880 通信 ， 1.516010 游戏
+        BankuaiData etfList = getLast30DayData("1.512200");
+        BankuaiData etf300List = getLast30DayData("1.515790");
 
         int lastZhangDieTingCount = 999;
-        double shouYiSum1 = 0;
-        double shouYiSum2 = 0;
-        double shouYiSum3 = 0;
-        double shouYiSum4 = 0;
-        for (int dayIndex = 1; dayIndex < list2list.get(0).size() - 1; dayIndex++) {
-            double bankuaiMingRiShouYi = etfList.get(dayIndex + 1).end / etfList.get(dayIndex).end - 1;
-            double bankuai300MingRiShouYi = etf300List.get(dayIndex + 1).end / etf300List.get(dayIndex).end - 1;
+        double celue1ShouYiSum = 1;
+        double allShiYanShouYiSum = 1;
+        double celue2ShouYiSum = 1;
+        double allDuiZhaoShouYiSum = 1;
+        for (int dayIndex = 1; dayIndex < etfList.detailList.size() - 1; dayIndex++) {
+            double bankuaiMingRiShouYi = etfList.detailList.get(dayIndex + 1).end / etfList.detailList.get(dayIndex).end - 1;
+            double bankuai300MingRiShouYi = etf300List.detailList.get(dayIndex + 1).end / etf300List.detailList.get(dayIndex).end - 1;
             bankuai300MingRiShouYi *= 1;
             int zhangting = 0;
             int dieting = 0;
             Set<String> dateSet = new HashSet<>();
-            String date = list2list.get(1).get(dayIndex).date;
+            String date = list2list.get(1).detailList.get(dayIndex).date;
             dateSet.add(date);
             for (int gupiaoIndex = 0; gupiaoIndex < list2list.size(); gupiaoIndex++) {
-                shangZheng.Main.OneDayDataDetail oneDay = list2list.get(gupiaoIndex).get(dayIndex);
+                shangZheng.Main.OneDayDataDetail oneDay = list2list.get(gupiaoIndex).detailList.get(dayIndex);
                 if (!dateSet.contains(oneDay.date)) {
                     System.out.println("日期有问题:" + gupiaoIndex);
+                    continue;
                 }
                 dateSet.add(oneDay.date);
-                shangZheng.Main.OneDayDataDetail lastDay = list2list.get(gupiaoIndex).get(dayIndex - 1);
-                if (oneDay.end / lastDay.end > 1.095) {
-                    zhangting++;
-                } else if (oneDay.end / lastDay.end < 0.905) {
-                    dieting++;
+                shangZheng.Main.OneDayDataDetail lastDay = list2list.get(gupiaoIndex).detailList.get(dayIndex - 1);
+                if (list2list.get(gupiaoIndex).code.startsWith("0")) {
+                    if (oneDay.end / lastDay.end > 1.19) {
+                        zhangting++;
+                    } else if (oneDay.end / lastDay.end < 0.81) {
+                        dieting++;
+                    }
+                } else {
+                    if (oneDay.end / lastDay.end > 1.095) {
+                        zhangting++;
+                    } else if (oneDay.end / lastDay.end < 0.905) {
+                        dieting++;
+                    }
                 }
             }
+
             int zhangdieTingCount = zhangting - dieting;
-            String color = ANSI_RESET;
+            String color;
+            String desc;
             if (zhangdieTingCount >= lastZhangDieTingCount) {
                 color = ANSI_RED;
-                shouYiSum1 += bankuaiMingRiShouYi;
+                desc = "买入";
+                celue1ShouYiSum *= 1 + bankuaiMingRiShouYi;
             } else {
-                shouYiSum1 += bankuai300MingRiShouYi;
                 color = ANSI_GREEN;
+                desc = "不买";
+                celue1ShouYiSum *= 1 + bankuai300MingRiShouYi;
             }
-            shouYiSum2 += bankuaiMingRiShouYi;
-            shouYiSum4 += bankuai300MingRiShouYi;
+            allShiYanShouYiSum *= 1 + bankuaiMingRiShouYi;
+            allDuiZhaoShouYiSum *= 1 + bankuai300MingRiShouYi;
             if (zhangdieTingCount >= 0) {
-                shouYiSum3 += bankuaiMingRiShouYi;
+                celue2ShouYiSum *= 1 + bankuaiMingRiShouYi;
             } else {
-                shouYiSum1 += bankuai300MingRiShouYi;
+                celue2ShouYiSum *= 1 + bankuai300MingRiShouYi;
             }
-            String desc = zhangdieTingCount > lastZhangDieTingCount ? "买入" : "不买";
             lastZhangDieTingCount = zhangdieTingCount;
             System.out.printf(color + "日期：%s,涨停: %d ,跌停: %d %s， 明日收益：%.2f%% ," +
-                            "策略总收益：%.2f%% ,无脑总收益：%.2f%%,策略2总收益：%.2f%%,300 总收益：%.2f%% \n" + ANSI_RESET,
-                    date, zhangting, dieting, desc, bankuaiMingRiShouYi * 100, shouYiSum1 * 100, shouYiSum2 * 100, shouYiSum3 * 100, shouYiSum4 * 100);
+                            "策略1总收益：%.2f%% ,全实验组总收益：%.2f%% ,策略2总收益：%.2f%% ,全对照组总收益：%.2f%% \n" + ANSI_RESET,
+                    date, zhangting, dieting, desc, bankuaiMingRiShouYi * 100, celue1ShouYiSum * 100 - 100,
+                    allShiYanShouYiSum * 100 - 100, celue2ShouYiSum * 100 - 100, allDuiZhaoShouYiSum * 100 - 100);
         }
         System.out.println("成功:" + successSet);
         System.out.println("失败：" + failSet);
@@ -90,16 +104,23 @@ public class ZhangTing {
     static Map<String, String> successCodePrefixMap = new HashMap<>();
 
     static {
-        String successCodeJSONArr = "1.600, 1.688, 1.512, 1.601, 1.603, 1.000, 1.561, 0.300, 0.301";
+        String successCodeJSONArr = "1.600, 1.688, 1.512, 1.601, 1.603, 1.000, 1.561, 0.300, 0.301, 90.BK0, 90.BK1";
         Arrays.stream(successCodeJSONArr.split(", ")).forEach(sub -> {
             String[] sub2 = sub.split("\\.");
             successCodePrefixMap.put(sub2[1], sub2[0]);
         });
     }
 
+    @AllArgsConstructor
+    public static class BankuaiData {
+        String code;
+        String name;
+        List<shangZheng.Main.OneDayDataDetail> detailList;
+    }
+
     //    https://push2his.eastmoney.com/api/qt/stock/kline/get?fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61&beg=0&end=20500101&ut=fa5fd1943c7b386f172d6893dbfba10b&rtntype=6&secid=1.000558&klt=101&fqt=1&cb=jsonp1732998219242
 //    https://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery35105715717072793236_1732997335203&secid=1.000558&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&end=20500101&lmt=120&_=1732997335253
-    private static List<shangZheng.Main.OneDayDataDetail> getLast30DayData(String bankuaiCode) {
+    private static BankuaiData getLast30DayData(String bankuaiCode) {
         JSONArray jsonArray = null;
         try {
             jsonArray = Main.getDayData(bankuaiCode);
@@ -118,7 +139,7 @@ public class ZhangTing {
 //        System.out.println("成功" + bankuaiCode);
         List<String> list = jsonArray.stream().map(e -> (String) e).collect(Collectors.toList());
         List<shangZheng.Main.OneDayDataDetail> detailList = Utils.parseDongFangCaiFuList(list);
-        return detailList.subList(detailList.size() - 90, detailList.size() - 10);
+        return new BankuaiData(bankuaiCode, "", detailList.subList(detailList.size() - 50, detailList.size() - 1));
     }
 
     public static final String ANSI_RESET = "\u001B[0m";
@@ -126,3 +147,6 @@ public class ZhangTing {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
 }
+
+//查板块个股
+//https://33.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112404930637717184252_1733073559136&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&dect=1&wbp2u=|0|0|0|web&fid=f3&fs=b:BK1036+f:!50&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152,f45&_=1733073559152
